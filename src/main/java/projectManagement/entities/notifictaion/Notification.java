7416f02c2429c6704b29f7134ac32be0d5178089
talbenzur <tbz1996@gmail.com>
2022-12-14 21:49:01 +0200

    add all classes from class diagram uml to the project add value to enum add Notification class variable

--- src/main/java/projectManagement/entities/notifictaion/Notification.java
+++ src/main/java/projectManagement/entities/notifictaion/Notification.java
@@ -1,4 +1,13 @@
 package projectManagement.entities.notifictaion;
 
+import java.time.LocalDate;
+
 public class Notification {
+    Long id;
+    Long userId;
+    Long assignerId;
+    Long boardId;
+    LocalDate date;
+    NotificationType notificationType;
+    //T data;
 }
